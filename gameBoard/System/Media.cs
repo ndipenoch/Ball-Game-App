c07864389870dd332d5e216ed4c44abb5d25f945
NCHONGMAJE NDIPENOCH - STUDENT <G00352031@gmit.ie>
2017-12-08 12:07:35 +0000

    first commit

---
+++ gameBoard/System/Media.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    internal class Media
+    {
+        internal class SoundPlayer
+        {
+            private string v;
+
+            public SoundPlayer(string v)
+            {
+                this.v = v;
+            }
+        }
+    }
+}
